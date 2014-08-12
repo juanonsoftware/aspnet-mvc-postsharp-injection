522959db6e1b0f410858c52a3d7ff7b1621311bc
Huan <hhoangvan@pentalog.fr>
2014-08-12 17:27:16 +0700

    Default

---
+++ PostsharpAndInjection/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PostsharpAndInjection
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
